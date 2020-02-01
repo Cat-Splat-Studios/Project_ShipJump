e006e765123e2db881351f933a5fb99fc2a7a439
Matthew Douglas <matt.w.doug@gmail.com>
2020-02-01 17:24:25 -0500

    Collisions

--- Assets/Player/Scripts/PlayerMovement.cs
+++ Assets/Player/Scripts/PlayerMovement.cs
@@ -4,27 +4,29 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public bool hasFuel = true;
-
+    // Player Speeds
     public float speedUp = 2.0f;
     public float speedX = 2.0f;
-
-
+    private float currentSpeedUp;
+    private float currentSpeedX;
+    
+    // Player Fuel
     public float MaxFuel = 100.0f;
-    public float currentFuel;
     public float fuelDecrease = 1.0f;
+    private  float currentFuel;
 
+    // Player Distance
+    private float distance;
+    private float startYpos;
+
+    // References
     private Rigidbody rb;
     private UIDelgate ui;
 
-    private float currentSpeedUp;
-    private float currentSpeedX;
-
+    // Misc
+    private bool canMove = true;
     private float screenCenterX;
-
     private bool canSwitchCam = true;
-
-    private float distance;
 
     // Start is called before the first frame update
     void Start()
@@ -38,79 +40,122 @@ public class PlayerMovement : MonoBehaviour
 
         currentSpeedX = 0.0f;
 
-
+        startYpos = transform.position.y;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Application.isMobilePlatform)
+        if(canMove)
         {
-            if (Input.touchCount > 0)
+            if (Application.isMobilePlatform)
             {
-                // get the first one
-                Touch firstTouch = Input.GetTouch(0);
-
-                // if it began this frame
-                if (firstTouch.phase == TouchPhase.Stationary)
+                if (Input.touchCount > 0)
                 {
-                    if (firstTouch.position.x > screenCenterX)
+                    // get the first one
+                    Touch firstTouch = Input.GetTouch(0);
+
+                    // if it began this frame
+                    if (firstTouch.phase == TouchPhase.Stationary)
+                    {
+                        if (firstTouch.position.x > screenCenterX)
+                        {
+                            currentSpeedX = speedX;
+                        }
+                        else if (firstTouch.position.x < screenCenterX)
+                        {
+                            currentSpeedX = -speedX;
+                        }
+                    }
+                }
+                else
+                {
+                    currentSpeedX = 0;
+                }
+            }
+
+            if (Application.isEditor)
+            {
+                if (Input.GetMouseButton(0))
+                {
+                    if (Input.mousePosition.x > screenCenterX)
                     {
                         currentSpeedX = speedX;
                     }
-                    else if (firstTouch.position.x < screenCenterX)
+                    else if (Input.mousePosition.x < screenCenterX)
                     {
                         currentSpeedX = -speedX;
                     }
                 }
+                else
+                {
+                    currentSpeedX = 0;
+                }
+
+            }
+
+
+            if (currentFuel > 0.0f)
+            {
+                currentSpeedUp = speedUp;
             }
             else
             {
-                currentSpeedX = 0;
-            }
-        }
-
-        if (Application.isEditor)
-        {
-            if (Input.GetMouseButton(0))
-            {
-                if (Input.mousePosition.x > screenCenterX)
+                currentSpeedUp = 0.0f;
+                currentFuel = 0.0f;
+                if (canSwitchCam)
                 {
-                    currentSpeedX = speedX;
+                    FindObjectOfType<CameraFollow>().SwitchCameraOffset();
+                    canSwitchCam = false;
                 }
-                else if (Input.mousePosition.x < screenCenterX)
-                {
-                    currentSpeedX = -speedX;
-                }
-            }
-            else
-            {
-                currentSpeedX = 0;
-            }
-    
-        }
-      
 
-        if (hasFuel && currentFuel > 0.0f)
+            }
+
+            currentFuel -= fuelDecrease * Time.deltaTime;
+            rb.velocity = new Vector3(currentSpeedX, currentSpeedUp, 0.0f);
+            transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.0f, 2.0f), transform.position.y, transform.position.z);
+
+            ui.curFuel = currentFuel / 100;
+            distance = Mathf.Round(transform.position.y - startYpos);
+            ui.curDistance = distance.ToString();
+        }     
+    }
+
+    public void AddFuel(float amount)
+    {
+        currentFuel += amount;
+        if (currentFuel > MaxFuel)
         {
-            currentSpeedUp = speedUp;
+            currentFuel = MaxFuel;
+        }
+    }
+
+    public void Boost()
+    {
+
+    }
+
+    public void StopMovement()
+    {
+        canMove = false;
+
+        ScoreCheck();
+    }
+
+    private void ScoreCheck()
+    {
+        if(PlayerPrefs.HasKey("highscore"))
+        {
+            float highscore = PlayerPrefs.GetFloat("highscore");
+
+            if (distance > highscore)
+            {
+                PlayerPrefs.SetFloat("highscore", distance);
+            }
         }
         else
         {
-            currentSpeedUp = 0.0f;
-            currentFuel = 0.0f;
-            if(canSwitchCam)
-            {
-                FindObjectOfType<CameraFollow>().SwitchCameraOffset();
-                canSwitchCam = false;
-            }
-            
+            PlayerPrefs.SetFloat("highscore", distance);
         }
-
-        currentFuel -= fuelDecrease * Time.deltaTime;
-        rb.velocity = new Vector3(currentSpeedX, currentSpeedUp, 0.0f);
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -2.0f, 2.0f), transform.position.y, transform.position.z);
-
-        ui.curFuel = currentFuel / 100;
     }
 }
