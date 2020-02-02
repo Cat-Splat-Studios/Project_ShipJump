90bb8caf4ab0228aad9b60c1aa4d1bb22308a96b
AnagramMC <hisham.ata@hotmail.com>
2020-02-01 20:05:25 -0500

    background scrolling in - experimental

---
+++ Assets/Background/RotateSky.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateSky : MonoBehaviour
+{
+    float length, startPOS;
+    public GameObject camera;
+    public float scrollEffect;
+
+    void Start()
+    {
+        camera = FindObjectOfType<Camera>().gameObject;
+        startPOS = transform.position.y;
+        length = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    void Update()
+    {
+        float temp = camera.transform.position.y * (1 - scrollEffect);
+        float distance = camera.transform.position.y * scrollEffect;
+
+        transform.position = new Vector3(transform.position.x, startPOS + distance, transform.position.z);
+
+        if (temp > (startPOS + length))
+        {
+            startPOS += length;
+        }
+        else if (temp < (startPOS - length))
+        {
+            startPOS -= length;
+        }
+    }
+}
