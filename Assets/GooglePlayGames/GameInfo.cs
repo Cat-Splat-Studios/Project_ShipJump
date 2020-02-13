c3746e9b20fed0ebc4dd7bc7dbda12e32e1cb4c1
AnagramMC <hisham.ata@hotmail.com>
2020-02-12 19:44:18 -0500

    inapp purchases and billing linked to google play

--- Assets/GooglePlayGames/GameInfo.cs
+++ Assets/GooglePlayGames/GameInfo.cs
@@ -38,7 +38,7 @@ namespace GooglePlayGames {
 
         public const string ApplicationId = "273189738208"; // Filled in automatically
         public const string IosClientId = "__IOS_CLIENTID__"; // Filled in automatically
-        public const string WebClientId = ""; // Filled in automatically
+        public const string WebClientId = "273189738208-uvka5n1ipplu41a0c8pah34dt6p3qams.apps.googleusercontent.com"; // Filled in automatically
         public const string NearbyConnectionServiceId = "";
 
         public static bool ApplicationIdInitialized() {
